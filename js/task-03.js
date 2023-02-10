b38f4b710ce7f7752a8972b43740891affe2dc20
Lisa Riabokon <lisa.checheleva@gmail.com>
2023-02-10 13:10:42 +0200

    add base to task-03

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,35 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const imagesContainerEl = document.querySelector("gallery");
+
+const galleryItemEl = document.createElement("li");
+
+const imageEl = document.createElement("img");
+imageEl.url = images.url;
+imageEl.alt = images.alt;
+console.log(imageEl);
+
+galleryItemEl.appendChild(imageEl);
+console.log(galleryItemEl);
+
+// const galleryImage = ({ url, alt }) => {
+// url, alt;
+// };
+
+// const makeGalleryCardImage = images.map(galleryImage).join("");
+
+// imagesContainerEl.insertAdjacentHTML("afterbegin", makeGalleryCardImage);
+// console.log(makeGalleryCardImage);
