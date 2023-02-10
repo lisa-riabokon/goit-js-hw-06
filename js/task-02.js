dfa1e0633e0be906588a40eb085bbd64c09c5a40
Lisa Riabokon <lisa.checheleva@gmail.com>
2023-02-10 12:16:21 +0200

    add task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsContainerEl = document.querySelector("#ingredients");
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement()
+ingredients.forEach((ingredientItem) => {
+  const ingredientEl = document.createElement("li");
+  // Додасть назву інгредієнта як його текстовий вміст
+  ingredientEl.textContent = ingredientItem;
+  // Додасть елементу клас item
+  ingredientEl.classList.add("item");
+  // Після чого, вставить усі <li> за одну операцію у список ul#ingredients
+  ingredientsContainerEl.append(ingredientEl);
+});
